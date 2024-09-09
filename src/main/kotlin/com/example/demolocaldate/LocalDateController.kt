e89047ce1fe569088f081c59fe0e11eea0af3cf4
Artur Bielecki <artur.bielecki@mamf.pl>
2024-09-09 11:50:42 +0200

    Comparison

--- src/main/kotlin/com/example/demolocaldate/LocalDateController.kt
+++ src/main/kotlin/com/example/demolocaldate/LocalDateController.kt
@@ -1,5 +1,8 @@
 package com.example.demolocaldate
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/add-day")
-class LocalDateController {
+class LocalDateController(
+    private val springObjectMapper: ObjectMapper
+) {
+    private val myObjectMapper = ObjectMapper()
 
     @PostMapping
     fun addDay(
@@ -15,4 +21,37 @@ class LocalDateController {
     ) = LocalDateDto(
         date = date.date.plusDays(1)
     )
+
+    @PostMapping(
+        "/working",
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun addDaySpringObjectMapper(
+        @RequestBody body: String
+    ): String {
+        val input: LocalDateDto = springObjectMapper.readValue(body)
+        return springObjectMapper.writeValueAsString(
+            LocalDateDto(
+                date = input.date.plusDays(1)
+            )
+        )
+    }
+
+    @PostMapping(
+        "/not-working",
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun addDayCreatedObjectMapper(
+        @RequestBody body: String
+    ): String {
+        val input: LocalDateDto = myObjectMapper.readValue(body)
+        return myObjectMapper.writeValueAsString(
+            LocalDateDto(
+                date = input.date.plusDays(1)
+            )
+        )
+    }
+
 }
